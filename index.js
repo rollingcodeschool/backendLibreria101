5910c0cae02bf0eca8d4ae6fde07380acee6fb16
Emilse Arias <emilsearias@gmail.com>
2024-09-26 15:34:55 -0300

    agregue el puerto a index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from 'express'
 
-console.log('Hola mundo')
 //1 - configurar un puerto
+const app = express();
+
+app.set('port', process.env.PORT || 4000)
+app.listen(app.get('port'), ()=>{
+    console.info('Estoy escuchando el puerto '+app.get('port'))
+})
 
 //2 - configurar middlewares
 
