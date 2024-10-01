dda127515aeb0c077e9fbb6b77fe8e2453c4d4e9
Emilse Arias <emilsearias@gmail.com>
2024-10-01 15:19:03 -0300

    agregamos un index.html como archivo estatico

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import morgan from 'morgan';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 //1 - configurar un puerto
 const app = express();
@@ -15,6 +17,9 @@ app.use(cors());// permite conexiones remotas
 app.use(morgan('dev')); //nos da informacion extra en la terminal
 app.use(express.json());//interpretar los datos en formato json de la solicitud
 app.use(express.urlencoded({extended:true}))
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+app.use(express.static(path.join(__dirname,"/public")))
 
 //3 - configurar las rutas
 // http://localhost:4000/prueba
