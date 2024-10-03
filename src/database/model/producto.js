32cf9aba397ada6769bde7a1f65a8a123c558b35
Emilse Arias <emilsearias@gmail.com>
2024-10-03 14:46:10 -0300

    agregamos el modelo de producto

--- src/database/model/producto.js
+++ src/database/model/producto.js
@@ -1,17 +1,53 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const productoSchema = new Schema({
-    nombreProducto:{
-        type: String,
-        required: true,
-        minLength: 2,
-        maxLength: 100,
-        unique:true
+  nombreProducto: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 100,
+    unique: true,
+  },
+  precio: {
+    type: Number,
+    required: true,
+    min: 50,
+    max: 1000000,
+  },
+  imagen: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (valor) => {
+        return /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/.test(valor);
+      },
     },
-    precio:{
-        type:Number,
-        required:true,
-        min:50,
-        max:1000000
-    }
-})
+  },
+  categoria: {
+    type: String,
+    required: true,
+    enum: ["Arte", "Cuadernos", "Escritura", "Hojas"],
+  },
+  marca: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 50,
+  },
+  descripcion_breve: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 50,
+  },
+  descripcion_amplia: {
+    type: String,
+    required: true,
+    minLength: 50,
+    maxLength: 1000,
+  },
+});
+
+const Producto = mongoose.model("producto", productoSchema);
+
+export default Producto;
