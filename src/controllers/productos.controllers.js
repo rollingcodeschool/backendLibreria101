d83338ba0a0dc7f020b9d5d47478da585e2a8d9e
Emilse Arias <emilsearias@gmail.com>
2024-10-03 16:28:11 -0300

    agregamos la funcion para crearProductos sin validaciones

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,4 +1,24 @@
+import Producto from "../database/model/producto.js"
+
+Producto
 export const funcionPrueba = (req, res)=>{
     console.log('alguien hizo una solicitud get a la ruta de prueba')
     res.send('Hola mundo desde el backend')
+}
+
+export const crearProducto = async(req, res)=>{
+   try {
+    //extraer el producto del body de la solicitud (req)
+    //validar los datos del body
+    //crear un objeto con el modelo producto Producto
+    const productoNuevo = new Producto(req.body);
+    //guardar el objeto en la BD
+    await productoNuevo.save()
+    //enviar la respuesta que pudimos crear el producto
+    res.status(201).json({mensaje:'El producto fue creado correctamente'})
+   } catch (error) {
+    console.error(error)
+    res.status(500).json({mensaje: 'Ocurrio un error, no se pudo crear el producto'})
+   }
+   
 }
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { funcionPrueba } from "../controllers/productos.controllers.js";
+import { crearProducto, funcionPrueba } from "../controllers/productos.controllers.js";
 
 const router = Router();
-router.route('/prueba').get(funcionPrueba)
+router.route('/prueba').get(funcionPrueba);
+router.route('/productos').post(crearProducto)
 
 export default router
