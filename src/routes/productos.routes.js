3211678bb62884312b3e5ee426df48e9b6e78452
Emilse Arias <emilsearias@gmail.com>
2024-10-08 16:40:45 -0300

    agregue formato en el archivo producto.routes

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,20 @@
 import { Router } from "express";
-import { borrarProducto, crearProducto, editarProducto, funcionPrueba, listarProductos, obtenerProducto } from "../controllers/productos.controllers.js";
+import {
+  borrarProducto,
+  crearProducto,
+  editarProducto,
+  funcionPrueba,
+  listarProductos,
+  obtenerProducto,
+} from "../controllers/productos.controllers.js";
 
 const router = Router();
-router.route('/prueba').get(funcionPrueba);
-router.route('/productos').post(crearProducto).get(listarProductos)
-router.route('/productos/:id').put(editarProducto).delete(borrarProducto).get(obtenerProducto)
+router.route("/prueba").get(funcionPrueba);
+router.route("/productos").post(crearProducto).get(listarProductos);
+router
+  .route("/productos/:id")
+  .put(editarProducto)
+  .delete(borrarProducto)
+  .get(obtenerProducto);
 
-export default router
+export default router;
